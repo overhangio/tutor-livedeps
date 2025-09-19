36e9f85b33381970be09b8a74409c0701d556166
mlabeeb03 <mlabeeb03@gmail.com>
2025-09-19 23:02:06 +0500

    feat: use uwsgi cron to run monitor script and add a last_update_timestamp file Use the uwsgi cron option to run the monitor_livedeps script instead of a daemon that runs all the time. To get the local timestamp use the last_update_timestamp file instead of the uwsgi_trigger file..

--- tutorlivedeps/templates/livedeps/build/monitor_livedeps.py
+++ tutorlivedeps/templates/livedeps/build/monitor_livedeps.py
@@ -1,26 +1,34 @@
+"""
+This script is executed as a cron job every minute. It checks if the remote
+`deps.zip` file has been updated. If it has, it touches the  uwsgi_trigger file
+that cause uWSGI to reload the application.
+"""
+
 import datetime
-import time
+import os
 
 from django.core.files.storage import storages
 
 DEPS_KEY = "deps.zip"
 TRIGGER_FILE = "/openedx/live-dependencies/uwsgi_trigger"
+TIMESTAMP_FILE = "/openedx/live-dependencies/last_update_timestamp"
 
-# TODO Use a separate storage for live dependencies
-storage = storages["default"]
 
-while True:
+def main():
+    # TODO Use a separate storage for live dependencies
+    storage = storages["default"]
+
     if storage.exists(DEPS_KEY):
         remote_ts = storage.get_modified_time(DEPS_KEY)
-        local_ts = None
-        with open(TRIGGER_FILE, "r") as f:
+        with open(TIMESTAMP_FILE, "r") as f:
             local_ts_str = f.read().strip()
             if local_ts_str:
                 local_ts = datetime.datetime.fromisoformat(local_ts_str)
 
-        if local_ts is None or local_ts < remote_ts:
-            now = datetime.datetime.now(tz=datetime.timezone.utc)
-            with open(TRIGGER_FILE, "w") as f:
-                # Writing to the TRIGGER_FILE will cause uWSGI to reload the app
-                f.write(now.isoformat())
-    time.sleep(10)
+        if local_ts < remote_ts:
+            with open(TRIGGER_FILE, "a"):
+                os.utime(TRIGGER_FILE, None)
+
+
+if __name__ == "__main__":
+    main()
